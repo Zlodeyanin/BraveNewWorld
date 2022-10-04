31ebcbf7539129559a66145283a425b516aec503
Zlodetanin <zlodeyanin0@gmail.com>
2022-10-04 22:39:24 +0300

    Добавьте файлы проекта.

---
+++ BraveNewWorld55/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BraveNewWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char border = '#';
+            char treasure = 'S';
+            int playerPositionX;
+            int playerPositionY;
+            int playerDirectionX = 0;
+            int playerDitectionY = 0;
+            bool isGame = true;
+            Console.CursorVisible = false;
+            char[,] map = ReadMap("map1", out playerPositionX, out playerPositionY);
+            DrawMap(map);
+
+            while (isGame)
+            {
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    ChangeDirecrion(key, ref playerDirectionX, ref playerDitectionY);
+
+                    if (map[playerPositionX + playerDirectionX, playerPositionY + playerDitectionY] != border)
+                    {
+                        Move(ref playerPositionX, ref playerPositionY, playerDirectionX, playerDitectionY);
+                    }
+
+                    if (map[playerPositionX, playerPositionY] == treasure)
+                    {
+                        Console.SetCursorPosition(0, 20);
+                        Console.WriteLine("Вы нашли сокровище!");
+                        Console.ReadKey();
+                        isGame = false;                        
+                    }
+                }              
+            }
+        }
+
+        static void Move(ref int playerPositionX, ref int playerPositionY, int playerDirectionX, int playerDitectionY)
+        {
+            char player = '@';
+            Console.SetCursorPosition(playerPositionY, playerPositionX);
+            Console.Write(" ");
+            playerPositionX += playerDirectionX;
+            playerPositionY += playerDitectionY;
+            Console.SetCursorPosition(playerPositionY, playerPositionX);
+            Console.Write(player);
+        }
+
+        static void ChangeDirecrion(ConsoleKeyInfo key, ref int playerDirectionX, ref int playerDitectionY)
+        {
+            const ConsoleKey upArrow = ConsoleKey.UpArrow;
+            const ConsoleKey downArrow = ConsoleKey.DownArrow;
+            const ConsoleKey leftArrow = ConsoleKey.LeftArrow;
+            const ConsoleKey rightArrow = ConsoleKey.RightArrow;
+            playerDirectionX = 0;
+            playerDitectionY = 0;
+
+            switch (key.Key)
+            {
+                case upArrow:
+                    playerDirectionX = -1;
+                    playerDitectionY = 0;
+                    break;
+                case downArrow:
+                    playerDirectionX = 1;
+                    playerDitectionY = 0;
+                    break;
+                case leftArrow:
+                    playerDirectionX = 0;
+                    playerDitectionY = -1;
+                    break;
+                case rightArrow:
+                    playerDirectionX = 0;
+                    playerDitectionY = 1;
+                    break;
+
+            }
+        }
+
+
+        static void DrawMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        static char[,] ReadMap(string mapName, out int playerPositionX, out int playerPositionY)
+        {
+            char player = '@';
+            playerPositionX = 0;
+            playerPositionY = 0;
+            string[] newFile = File.ReadAllLines($"Maps/{mapName}.txt");
+            char[,] map = new char[newFile.Length, newFile[0].Length];
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = newFile[i][j];
+
+                    if (map[i, j] == player)
+                    {
+                        playerPositionX = i;
+                        playerPositionY = j;
+                    }
+                }
+            }
+            return map;
+        }
+    }
+}
---
+++ BraveNewWorld55/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BraveNewWorld55")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BraveNewWorld55")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("035a3c13-946c-4dd7-9853-4b46eb2eaebf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
